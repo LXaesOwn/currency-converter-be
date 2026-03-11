fa4037e0805ff9cc1bbce9379a84fcf07705d4e0
Daniil <dmitrievicdaniil32@gmail.com>
2026-03-11 12:42:02 +0300

    Remove sensitive and build files from git

--- dist/modules/user/user.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateUserSchema = void 0;
-
-const zod_1 = require("zod");
-exports.updateUserSchema = zod_1.z.object({
-    base_currency: zod_1.z.string().length(3).optional(),
-    favorites: zod_1.z.array(zod_1.z.string().length(3)).optional(),
-});
